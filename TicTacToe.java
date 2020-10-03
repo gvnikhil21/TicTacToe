68654b34d09ae6188d95b4105af15ea21ffb4fdb
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-10-03 21:06:49 +0530

    Added: Determining after every move it's tie or win or changeInTurn functionality added

--- TicTacToe.java
+++ TicTacToe.java
@@ -11,25 +11,32 @@ public class TicTacToe {
 	private static char playerLetter;
 
 	public static void main(String[] args) {
+		// variables
+		int isPlayerTurn;
+		char board[];
 		// welcome message
 		System.out.println("Welcome to Tic-Tac-Toe Game!");
 
 		// creates board
 		TicTacToe ticTacToe = new TicTacToe();
-		char board[] = ticTacToe.creatBoard();
+		board = ticTacToe.creatBoard();
 
 		// decides letter for player
 		ticTacToe.decideLetterByPlayer();
 
-		// decides first turn by toss
+		/* decides first turn by toss 
+		 * HEAD: Player plays first, else computer plays
+		 * first isPlayerTurn=1->player turn, else computer turn
+		 */
 		int tossResult = ticTacToe.tossDecideFirstTurn();
 		if (tossResult == HEAD) {
 			System.out.println("Player plays first");
-			ticTacToe.makeMoveUser(board);
+			isPlayerTurn = 1;
 		} else {
 			System.out.println("Computer plays first");
-			ticTacToe.makeMoveForComputer(board);
+			isPlayerTurn = 0;
 		}
+		ticTacToe.makeMove(isPlayerTurn, board);
 	}
 
 	// create board of length 10 & initialize indices except 0th-index with space
@@ -70,6 +77,30 @@ public class TicTacToe {
 		System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
 	}
 
+	// making move which decided and calls move for user or computer
+	private void makeMove(int isPlayerTurn, char[] board) {
+		int boardFilled = 0;
+		while (boardFilled < 9 && isWinner(board) == false) {
+			if (isPlayerTurn == 1) {
+				makeMoveUser(board);
+				isPlayerTurn = 0;
+			} else {
+				makeMoveForComputer(board);
+				isPlayerTurn = 1;
+			}
+			if (isWinner(board) && isPlayerTurn == 1) {
+				System.out.println("Computer has Won!");
+				break;
+			} else if (isWinner(board) && isPlayerTurn == 0) {
+				System.out.println("Player has Won!");
+				break;
+			}
+			boardFilled++;
+		}
+		if (isWinner(board) == false)
+			System.out.println("It's a Draw! No one has won");
+	}
+
 	// making move for user
 	private void makeMoveUser(char[] board) {
 		int position = 0;
@@ -86,8 +117,9 @@ public class TicTacToe {
 
 	// making move for computer
 	private void makeMoveForComputer(char[] board) {
-		int position = (int) Math.floor(Math.random() * 9) + 1;
+		int position = 0;
 		do {
+			position = (int) Math.floor(Math.random() * 9) + 1;
 			if (position > 0 && position < 10 && isPositionFree(board, position)) {
 				moveToPosition(position, board, computerLetter);
 				break;
@@ -110,5 +142,17 @@ public class TicTacToe {
 	private int tossDecideFirstTurn() {
 		int tossResult = (int) Math.floor(Math.random() * 10) % 2;
 		return tossResult;
+	}
+
+	// check winner
+	private boolean isWinner(char[] board) {
+		return ((board[1] == board[2] && board[2] == board[3] && board[1] != ' ')
+				|| (board[4] == board[5] && board[5] == board[6] && board[4] != ' ')
+				|| (board[7] == board[8] && board[8] == board[9] && board[7] != ' ')
+				|| (board[1] == board[4] && board[4] == board[7] && board[1] != ' ')
+				|| (board[2] == board[5] && board[5] == board[8] && board[2] != ' ')
+				|| (board[3] == board[6] && board[6] == board[9] && board[3] != ' ')
+				|| (board[1] == board[5] && board[5] == board[9] && board[1] != ' ')
+				|| (board[3] == board[5] && board[5] == board[7]) && board[2] != ' ');
 	}
 }
