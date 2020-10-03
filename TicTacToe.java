cc2a38d015d01aee7982cef0e45131d79250e3f5
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-10-03 17:39:00 +0530

    Added: Welcome message added

---
+++ TicTacToe.java
@@ -0,0 +1,10 @@
+package com.bridgelabs.tictactoe;
+
+public class TicTacToe {
+
+	public static void main(String[] args) {
+		// welcome message
+		System.out.println("Welcome to Tic-Tac-Toe Game!");
+	}
+
+}
