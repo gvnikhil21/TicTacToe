5b7da8836830714175ec92f18f27a01589e54851
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-10-03 17:48:41 +0530

    Added: Create ticTacToe board functionality added

--- TicTacToe.java
+++ TicTacToe.java
@@ -5,6 +5,16 @@ public class TicTacToe {
 	public static void main(String[] args) {
 		// welcome message
 		System.out.println("Welcome to Tic-Tac-Toe Game!");
+
+		TicTacToe ticTacToe = new TicTacToe();
+		char board[] = ticTacToe.creatBoard();
 	}
 
+	// create board of length 10 & initialize indices except 0th-index with space
+	private char[] creatBoard() {
+		char board[] = new char[10];
+		for (int indexBoard = 0; indexBoard < board.length; indexBoard++)
+			board[indexBoard] = ' ';
+		return board;
+	}
 }
