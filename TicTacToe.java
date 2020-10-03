29ad621249958acdae426f8380fee0649b1a75e6
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-10-04 00:01:31 +0530

    Added: Determining after every move it's tie or win or changeInTurn functionality added

--- TicTacToe.java
+++ TicTacToe.java
@@ -153,6 +153,6 @@ public class TicTacToe {
 				|| (board[2] == board[5] && board[5] == board[8] && board[2] != ' ')
 				|| (board[3] == board[6] && board[6] == board[9] && board[3] != ' ')
 				|| (board[1] == board[5] && board[5] == board[9] && board[1] != ' ')
-				|| (board[3] == board[5] && board[5] == board[7]) && board[2] != ' ');
+				|| (board[3] == board[5] && board[5] == board[7]) && board[3] != ' ');
 	}
 }
