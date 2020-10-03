a5a545d9694ba17aed35264418b5dd0e2561011b
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-10-03 18:08:02 +0530

    Added: Accept letter from player and assign to both player and computer

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,20 +1,52 @@
 package com.bridgelabs.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToe {
+
+	private static Scanner sc = new Scanner(System.in);
+	private static char computerLetter;
+	private static char playerLetter;
 
 	public static void main(String[] args) {
 		// welcome message
 		System.out.println("Welcome to Tic-Tac-Toe Game!");
 
+		// creates board
 		TicTacToe ticTacToe = new TicTacToe();
 		char board[] = ticTacToe.creatBoard();
+
+		// decides letter for player
+		ticTacToe.decideLetterByPlayer();
+
 	}
 
 	// create board of length 10 & initialize indices except 0th-index with space
 	private char[] creatBoard() {
 		char board[] = new char[10];
-		for (int indexBoard = 0; indexBoard < board.length; indexBoard++)
+		for (int indexBoard = 1; indexBoard < board.length; indexBoard++)
 			board[indexBoard] = ' ';
 		return board;
+	}
+
+	// deciding 'X' or 'O' for the player and computer
+	private void decideLetterByPlayer() {
+		playerLetter = acceptLetterFromThePlayer();
+		computerLetter = (playerLetter == 'X') ? 'O' : 'X';
+		System.out.println("Player letter is: " + playerLetter);
+		System.out.println("Computer letter is: " + computerLetter);
+	}
+
+	// accepting 'X' or 'O' from the player
+	private char acceptLetterFromThePlayer() {
+		char letterChosen = ' ';
+		do {
+			System.out.println("Enter 'X' or 'O' to choose the letter for playing: ");
+			letterChosen = sc.next().charAt(0);
+			if (letterChosen == 'X' || letterChosen == 'O')
+				break;
+			System.out.println("Enter valid letter either X or O");
+		} while (letterChosen != 'X' && letterChosen != 'O');
+		return letterChosen;
 	}
 }
